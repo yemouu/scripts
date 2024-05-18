1e613a83ce282fb09084b3df97c0b42a483c2390
yemou <dev@lilac.pink>
2024-05-18 19:12:25 -0400

    Add support for aarch64-linux to flake.nix
    Technically, all architectures that support Linux should work, but I
    don't have all the architectures on hand to actually test that.

--- flake.nix
+++ flake.nix
@@ -3,32 +3,33 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }: {
-    overlays.default = final: prev: rec {
-      colorgrab = final.callPackage ./.nix/colorgrab.nix { };
-      prefon = final.callPackage ./.nix/prefon.nix { };
-      scr = final.callPackage ./.nix/scr.nix { };
-      thm = final.callPackage ./.nix/thm.nix { };
-      yemou-scripts = final.symlinkJoin {
-        name = "yemou-scripts";
-        paths = [ colorgrab prefon scr thm ];
-      };
-    };
-
-    packages."x86_64-linux" =
-      let
-        pkgs = import nixpkgs {
-          system = "x86_64-linux";
+  outputs = { self, nixpkgs, ... }:
+    let
+      forAllSystems = function: nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ]
+        (system: function (import nixpkgs {
+          inherit system;
           overlays = [ self.overlays.default ];
+        }));
+    in
+    {
+      overlays.default = final: prev: rec {
+        colorgrab = final.callPackage ./.nix/colorgrab.nix { };
+        prefon = final.callPackage ./.nix/prefon.nix { };
+        scr = final.callPackage ./.nix/scr.nix { };
+        thm = final.callPackage ./.nix/thm.nix { };
+        yemou-scripts = final.symlinkJoin {
+          name = "yemou-scripts";
+          paths = [ colorgrab prefon scr thm ];
         };
-      in
-      {
+      };
+
+      packages = forAllSystems (pkgs: {
         colorgrab = pkgs.colorgrab;
         default = pkgs.yemou-scripts;
         prefon = pkgs.prefon;
         scr = pkgs.scr;
         thm = pkgs.thm;
         yemou-scripts = pkgs.yemou-scripts;
-      };
-  };
+      });
+    };
 }
